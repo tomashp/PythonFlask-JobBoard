d0aff12efdc4da08846fd874d8f83647a74be047
tomashp <38956755+tomashp@users.noreply.github.com>
2019-03-11 22:20:49 +0100

    sql typo

--- jobs/app.py
+++ jobs/app.py
@@ -16,7 +16,7 @@ def open_connection():
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    cursor = connection.execute(sql, valuse)
+    cursor = connection.execute(sql, values)
     if commit == True:
         results = connection.commit()
     else:
